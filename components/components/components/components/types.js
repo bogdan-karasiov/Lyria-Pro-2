25458772a00e77200c79d2851fad3bca12fafcfa
bogdan-karasiov <volna4ydahi@gmail.com>
2025-09-15 15:32:27 +0300

    Create types.js

---
+++ components/components/components/components/types.js
@@ -0,0 +1,6 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+*/
+// This file is intentionally left blank.
+// It previously contained TypeScript interfaces which are not used in JavaScript.
